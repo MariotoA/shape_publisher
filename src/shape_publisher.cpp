39de3ae18dabc75fe9a29740c621c654d53d5f85
MariotoA <malexrc7@gmail.com>
2018-04-24 17:35:05 +0200

    fixed typo

--- src/shape_publisher.cpp
+++ src/shape_publisher.cpp
@@ -3,9 +3,9 @@
 
 int main(int argc, char **argv)
 {
-	ros::init(argc,args,"shape_publisher");
+	ros::init(argc,argv,"shape_publisher");
 	ros::NodeHandle nh;
-	ros::Rate loop_rate(0.002);
+	ros::Rate loop_rate(20);
 	ros::Publisher pub = nh.advertise<geometry_msgs::Polygon>("/chassis_polygon",1000);
 	geometry_msgs::Polygon msg;
 	geometry_msgs::Point32 p1,p2,p3,p4;
@@ -21,9 +21,9 @@ int main(int argc, char **argv)
 		pub.publish(msg);
 		loop_rate.sleep();
 		ros::spinOnce();
-		//ROS_INFO("SHAPE PUBLISHED: P1[%f,%f]  P2[%f,%f]  P3[%f,%f]  P4[%f,%f]",
-		//p1.x,p1.y,p2.x,p2.y,p3.x,p3.y,p4.x,p4.y
-		//);
+		ROS_INFO("SHAPE PUBLISHED: P1[%f,%f]  P2[%f,%f]  P3[%f,%f]  P4[%f,%f]",
+		p1.x,p1.y,p2.x,p2.y,p3.x,p3.y,p4.x,p4.y
+		);
 	}
 	return 0;
 }
