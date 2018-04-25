b0e806aec8d094d855d36846fd29da41cdb42f34
MariotoA <malexrc7@gmail.com>
2018-04-25 18:50:01 +0200

    PoseStamped to be seen from rviz

--- src/shape_publisher.cpp
+++ src/shape_publisher.cpp
@@ -1,11 +1,14 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Polygon.h>
+#include <geometry_msgs/PolygonStamped.h>
 
 int main(int argc, char **argv)
 {
 	ros::init(argc,argv,"shape_publisher");
 	ros::NodeHandle nh;
-	ros::Rate loop_rate(20);
+	ros::Rate loop_rate(1);
+
+	//Polygon
 	ros::Publisher pub = nh.advertise<geometry_msgs::Polygon>("/chassis_polygon",1000);
 	geometry_msgs::Polygon msg;
 	geometry_msgs::Point32 p1,p2,p3,p4;
@@ -15,10 +18,17 @@ int main(int argc, char **argv)
 	msg.points.push_back(p2);
 	msg.points.push_back(p3);
 	msg.points.push_back(p4);
-	ROS_INFO("POINTS: %d", msg.points.size());
+
+	// Polygon Stamped (to see it on Rviz)
+	ros::Publisher pub_s = nh.advertise<geometry_msgs::PolygonStamped>("/chassis_polygon_stamped",1);
+	geometry_msgs::PolygonStamped msg_s;
+	msg_s.polygon = msg;
+	msg_s.header.stamp = ros::Time::now();
+	msg_s.header.frame_id = "base_link";
 	while (ros::ok())
 	{
 		pub.publish(msg);
+		pub_s.publish(msg_s);
 		loop_rate.sleep();
 		ros::spinOnce();
 		ROS_INFO("SHAPE PUBLISHED: P1[%f,%f]  P2[%f,%f]  P3[%f,%f]  P4[%f,%f]",
