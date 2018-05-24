747f4da3d9b6feb677da142159ff5c79321a398d
MariotoA <malexrc7@gmail.com>
2018-05-24 07:44:37 +0200

    Now you can send a shape via yaml. Also few changes more in the algorithm itself

--- src/shape_publisher.cpp
+++ src/shape_publisher.cpp
@@ -5,35 +5,59 @@
 int main(int argc, char **argv)
 {
 	ros::init(argc,argv,"shape_publisher");
-	ros::NodeHandle nh;
-	ros::Rate loop_rate(1);
+	ros::NodeHandle nh("~");
+	ros::Rate loop_rate(.5), loop_rate_rviz(10);
 
 	//Polygon
-	ros::Publisher pub = nh.advertise<geometry_msgs::Polygon>("/chassis_polygon",1000);
+	ros::Publisher pub = nh.advertise<geometry_msgs::Polygon>("/chassis_polygon",1);
 	geometry_msgs::Polygon msg;
-	geometry_msgs::Point32 p1,p2,p3,p4;
-	p4.x = p4.y = p3.y = p1.x = -0.225;
-	p2.x = p2.y = p3.x = p1.y = 0.225;
-	msg.points.push_back(p1);
-	msg.points.push_back(p2);
-	msg.points.push_back(p3);
-	msg.points.push_back(p4);
+
+	// Auxiliar var to store points temporarily.
+	std::vector<double> points;
+
+	// Obtaining parameters from yaml specified in rosparam tag
+	nh.getParam("shape", points);
+	ROS_INFO("MESSAGE CREATED");
+	for (int i=0; i < points.size(); i+=2)
+	{
+		geometry_msgs::Point32 p;
+		p.x = points[i];
+		p.y = points[i+1];
+		msg.points.push_back(p);
+		ROS_INFO("P%d[%f,%f]",i,p.x,p.y);
+	}
 
 	// Polygon Stamped (to see it on Rviz)
-	ros::Publisher pub_s = nh.advertise<geometry_msgs::PolygonStamped>("/chassis_polygon_stamped",1);
+	ros::Publisher pub_s = nh.advertise<geometry_msgs::PolygonStamped>("/chassis_polygon_stamped",1000);
 	geometry_msgs::PolygonStamped msg_s;
 	msg_s.polygon = msg;
 	msg_s.header.stamp = ros::Time::now();
 	msg_s.header.frame_id = "base_link";
+
+	// Timer starts
+	ros::Time begin = ros::Time::now();
+	while ( begin.toSec() == 0 ) 
+	{
+		begin = ros::Time::now();
+	}
+	double ellapsed_time = 0;
 	while (ros::ok())
 	{
-		pub.publish(msg);
-		pub_s.publish(msg_s);
-		loop_rate.sleep();
+		if (ellapsed_time < 5)
+		{
+			pub.publish(msg);
+			pub_s.publish(msg_s);
+			loop_rate.sleep();
+		} else 
+		{
+			pub_s.publish(msg_s);
+			loop_rate_rviz.sleep();
+		}
 		ros::spinOnce();
-		ROS_INFO("SHAPE PUBLISHED: P1[%f,%f]  P2[%f,%f]  P3[%f,%f]  P4[%f,%f]",
-		p1.x,p1.y,p2.x,p2.y,p3.x,p3.y,p4.x,p4.y
-		);
+		ros::Time current = ros::Time::now();
+		ellapsed_time = (current-begin).toSec();
+		ROS_INFO("SHAPE PUBLISHED.");
 	}
+	ROS_INFO("SHAPE PUBLISHER EXITING SAFELY.");
 	return 0;
 }
